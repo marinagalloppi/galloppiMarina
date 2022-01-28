70e52f855adc8cd589a77b22848857446e259bf4
marinagalloppi <92320531+marinagalloppi@users.noreply.github.com>
2022-01-28 18:30:18 +0100

    Create Galloppi-primo esercizio in python-2E.py

---
+++ Galloppi-primo esercizio in python-2E.py
@@ -0,0 +1,38 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:08:11) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 3
+>>> y = 4
+>>> 
+>>> print(x + y)
+7
+>>> x = 5
+>>> y = 5
+>>> 
+>>> print(x - y)
+0
+>>> x = 12
+>>> y = 6
+>>> 
+>>> print(x * y)
+72
+>>> x = 66
+>>> y = 11
+>>> 
+>>> print(x / y)
+6.0
+>>> x = 20
+>>> y = 6
+>>> 
+>>> print(x % y)
+2
+>>> x = 3
+>>> y = 5
+>>> 
+>>> print(x ** y)
+243
+>>> x = 16
+>>> y = 5
+>>> 
+>>> print(x // y)
+3
+>>> 
