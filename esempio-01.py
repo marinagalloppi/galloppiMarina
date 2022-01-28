7cd2e31f98067836d52b3e8effa9e91c70d754a5
marinagalloppi <92320531+marinagalloppi@users.noreply.github.com>
2022-01-28 18:30:01 +0100

    Create esempio-01.py

---
+++ esempio-01.py
@@ -0,0 +1,10 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:08:11) [MSC v.1928 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> lista = ["casa", "pappa", "scuola", "coding", "python"]
+>>> lista[0]
+'casa'
+>>> lista[3]
+'coding'
+>>> len(lista)
+5
+>>> 
