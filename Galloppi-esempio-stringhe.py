bfdf48efaafc0e1909e118cec440ae4d0b24d1ef
marinagalloppi <92320531+marinagalloppi@users.noreply.github.com>
2022-10-17 21:41:58 +0200

    Create Galloppi-esempio-stringhe.py

---
+++ Galloppi-esempio-stringhe.py
@@ -0,0 +1,20 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+stringa1 = 'Esempio stringa'
+stringa2 = ' Python'
+len(stringa1)
+15
+len(stringa2)
+7
+stringa1.upper()
+'ESEMPIO STRINGA'
+stringa1[0]
+'E'
+stringa1[-1]
+'a'
+stringa2[-5]
+'y'
+print(stringa1)
+Esempio stringa
+print(stringa1 + stringa2)
+Esempio stringa Python
