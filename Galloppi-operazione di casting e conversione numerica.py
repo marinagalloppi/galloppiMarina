43793626f386f6a9fa19b48ef3b07acfcefccb64
marinagalloppi <92320531+marinagalloppi@users.noreply.github.com>
2022-11-07 18:30:40 +0100

    Create Galloppi-operazione di casting e conversione numerica.py

---
+++ Galloppi-operazione di casting e conversione numerica.py
@@ -0,0 +1,17 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+oct(11)
+'0o13'
+x = 0o13
+print(x)
+11
+hex(123)
+'0x7b'
+y = 0x4bc
+print(y)
+1212
+bin(22)
+'0b10110'
+z = 0b11011
+print(z)
+27
